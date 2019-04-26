2e80be0d1b43b9a9a85f5383d38d7759739cf35e
rbaas293 <rybaas293@gmail.com>
2019-04-26 13:18:41 -0400

    added Jupyter cell comment/headers

--- code/_training/watermarking.py
+++ code/_training/watermarking.py
@@ -2,15 +2,17 @@
 # Original code from: https://www.blog.pythonlibrary.org/2017/10/17/how-to-watermark-your-photos-with-python/
 # Modifications made 4.26.2019 by Elizabeth Sheetz
 # pip install pillow
-
+# `#%%` - Signifies a runnable cell for use with `Jupyter Notebooks`
+ 
+#%% Imports
 from PIL import Image, ImageDraw, ImageFont
 import time
-
+#%% Variable Definitions:
 IMG_PATH = 'kitten.jpg'
 OUTPUT_PATH = 'kitten_watermarked_' + time.strftime("%Y%m%d-%H%M%S") + '.jpg'
 WATERMARK_TEXT = 'sheetz'
 FONT_FILE = "BERNHC.TTF"
-
+#%% Make The Watermark:
 def watermark_text(input_image_path,
                    output_image_path,
                    text, pos):
@@ -26,7 +28,7 @@ def watermark_text(input_image_path,
     photo.show()
     photo.save(output_image_path)
 
-
+#%% Honestly dont know what this does haha
 if __name__ == '__main__':
     watermark_text(IMG_PATH, OUTPUT_PATH,
                    text=WATERMARK_TEXT,
