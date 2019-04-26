8ec229cd2cdef83f28c5adbf49fd3608d57b56ee
Elizabeth Sheetz <ElizabethGSheetz@gmail.com>
2019-04-26 14:42:50 -0400

    Updated tensorflow example to actually work

--- code/_training/tensorflow_example.py
+++ code/_training/tensorflow_example.py
@@ -13,17 +13,37 @@ import matplotlib.pyplot as plt
 
 print(tf.__version__)
 
+
+# %%import fashion MNIST dataset
 fashion_mnist = keras.datasets.fashion_mnist
 
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
+
+# %%each image is mapped to a single label, but the human readable names are not included with the dataset, so we add them here
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
+
+# i was gonna try to modify stuff but nvm
 # for i in range(len(test_images)):
 #     WM.watermark_text(test_images[i], WM.OUTPUT_PATH, text=WM.WATERMARK_TEXT, pos=(0, 0))
 
 
+# %%uncomment this code to plot the first image, before preprocessing
+# plt.figure()
+# plt.imshow(train_images[0])
+# plt.colorbar()
+# plt.grid(False)
+# plt.show()
+
+
+# %%we need to scale the 0-255 values down to 0-1 before feeding them to the model
+train_images = train_images / 255.0
+
+test_images = test_images / 255.0
+
+# %%uncomment this code to plot the first 25 images
 # plt.figure(figsize=(10,10))
 # for i in range(25):
 #     plt.subplot(5,5,i+1)
@@ -34,28 +54,36 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
 #     plt.xlabel(class_names[train_labels[i]])
 #     plt.show()
 
+# %%Build the model -- setup the layers
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
     keras.layers.Dense(128, activation='relu'),
     keras.layers.Dense(10, activation='softmax')
 ])
-
+# %%Build the model -- compile the model
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
-
+# %%Train the Model
 model.fit(train_images, train_labels, epochs=5)
 
+# %%Evaluate Accuracy
 test_loss, test_acc = model.evaluate(test_images, test_labels)
 
 print('\nTest accuracy:', test_acc)
 
+
+# %%Make Predictions
 predictions = model.predict(test_images)
 
 print("Predictions: ", predictions[0])
 
-print("Highest confidence in label: ", np.argmax(predictions[0]))
+print("Highest confidence in label number: ", np.argmax(predictions[0]))
 
+print("The correct label number is: ", test_labels[0])
+
+
+# %% Function to plot the image with its correct/incorrect label and certainty
 def plot_image(i, predictions_array, true_label, img):
   predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
   plt.grid(False)
@@ -74,7 +102,7 @@ def plot_image(i, predictions_array, true_label, img):
                                 100*np.max(predictions_array),
                                 class_names[true_label]),
                                 color=color)
-
+# %% Function to plot the prediction array
 def plot_value_array(i, predictions_array, true_label):
   predictions_array, true_label = predictions_array[i], true_label[i]
   plt.grid(False)
@@ -87,7 +115,7 @@ def plot_value_array(i, predictions_array, true_label):
   thisplot[predicted_label].set_color('red')
   thisplot[true_label].set_color('blue')
 
-
+# %%Use the above functions to look at the 0th image, predictions, and prediction array
 i = 0
 plt.figure(figsize=(6,3))
 plt.subplot(1,2,1)
@@ -96,6 +124,7 @@ plt.subplot(1,2,2)
 plot_value_array(i, predictions,  test_labels)
 plt.show()
 
+# %%Use the above functions to look at the 12th image, predictions, and prediction array
 i = 12
 plt.figure(figsize=(6,3))
 plt.subplot(1,2,1)
@@ -104,6 +133,8 @@ plt.subplot(1,2,2)
 plot_value_array(i, predictions,  test_labels)
 plt.show()
 
+
+# %%Use the above functions to look at several images and predictions
 # Plot the first X test images, their predicted labels, and the true labels.
 # Color correct predictions in blue and incorrect predictions in red.
 num_rows = 5
@@ -117,6 +148,8 @@ for i in range(num_images):
   plot_value_array(i, predictions, test_labels)
 plt.show()
 
+
+# %%Finally, use the trained model to make a prediction about a single image
 # Grab an image from the test dataset.
 img = test_images[0]
 
@@ -126,6 +159,7 @@ img = (np.expand_dims(img,0))
 
 print(img.shape)
 
+# Now predict the correct label for this image:
 predictions_single = model.predict(img)
 
 print(predictions_single)
