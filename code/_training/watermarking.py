d9ed50455def951556d5fae70b1fba73bd4744ec
Elizabeth G Sheetz <sheetzeg@ceas1.uc.edu>
2019-04-26 13:00:45 -0400

    Added watermarking script

---
+++ code/_training/watermarking.py
@@ -0,0 +1,33 @@
+# This script designed to apply watermarks to photos.
+# Original code from: https://www.blog.pythonlibrary.org/2017/10/17/how-to-watermark-your-photos-with-python/
+# Modifications made 4.26.2019 by Elizabeth Sheetz
+# pip install pillow
+
+from PIL import Image, ImageDraw, ImageFont
+import time
+
+IMG_PATH = 'kitten.jpg'
+OUTPUT_PATH = 'kitten_watermarked_' + time.strftime("%Y%m%d-%H%M%S") + '.jpg'
+WATERMARK_TEXT = 'sheetz'
+FONT_FILE = "BERNHC.TTF"
+
+def watermark_text(input_image_path,
+                   output_image_path,
+                   text, pos):
+    photo = Image.open(input_image_path)
+
+    # make the image editable
+    drawing = ImageDraw.Draw(photo)
+
+    black = (3, 8, 12)
+    #font = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 40)
+    font = ImageFont.truetype(FONT_FILE, size=50)
+    drawing.text(pos, text, fill=black, font=font)
+    photo.show()
+    photo.save(output_image_path)
+
+
+if __name__ == '__main__':
+    watermark_text(IMG_PATH, OUTPUT_PATH,
+                   text=WATERMARK_TEXT,
+                   pos=(0, 0))
